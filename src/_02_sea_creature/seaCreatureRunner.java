aedc8ab7ad7fbd4549933271e078db28dc49b31b
Aditya521623 <54295036+Aditya521623@users.noreply.github.com>
2020-05-25 19:56:44 -0700

    worked on module 2

--- src/_02_sea_creature/seaCreatureRunner.java
+++ src/_02_sea_creature/seaCreatureRunner.java
@@ -4,38 +4,35 @@ public class seaCreatureRunner {
 
 	public static void main(String[] args) {
 		SeaCreature spongebob = new SeaCreature("Spongebob");
-		
+
 		SeaCreature squidward = new SeaCreature("Squidward");
 
 		SeaCreature Patrick = new SeaCreature("Patrick");
-		
+
 		String name = spongebob.getName();
-		
+
 		System.out.println(name);
-		
+
 		spongebob.eat();
-		
+
 		spongebob.laugh();
-		
-		
-String name2 = squidward.getName();
-		
+
+		String name2 = squidward.getName();
+
 		System.out.println(name2);
-		
+
 		squidward.eat();
-		
+
 		squidward.laugh();
 
-String name3 = Patrick.getName();
-		
+		String name3 = Patrick.getName();
+
 		System.out.println(name3);
-		
+
 		Patrick.eat();
-		
+
 		Patrick.laugh();
 
-
-		
 	}
 
 }
--- src/_03_smurf/smurfRunner.java
+++ src/_03_smurf/smurfRunner.java
@@ -3,28 +3,29 @@ package _03_smurf;
 public class smurfRunner {
 
 	public static void main(String[] args) {
-	Smurf handySmurf = new Smurf("Mr.Handy");
-	
-	Smurf PapaSmurf = new Smurf("Papa");
+		Smurf handySmurf = new Smurf("Handy Smurf");
 
-	Smurf Smurfette = new Smurf("Smurfette");
+		Smurf PapaSmurf = new Smurf("Papa Smurf");
 
-	String name = handySmurf.getName();
-	
-	System.out.println(name);
-	
-	handySmurf.eat();
+		Smurf Smurfette = new Smurf("Smurfette");
 
-	String name2 = PapaSmurf.getName();
-	
-	System.out.println(name);
-	
+		String name = handySmurf.getName();
 
+		System.out.println(name);
 
-	String name3 = Smurfette.getName();
-	
-	System.out.println(name);
+		handySmurf.eat();
 
+		String name2 = PapaSmurf.getName();
+
+		System.out.println(name2);
+
+		System.out.println("Papa Smurf has " + PapaSmurf.getHatColor());
+
+		String name3 = Smurfette.getName();
+
+		System.out.println(name3);
+
+		System.out.println("Smurfette is a " + Smurfette.isGirlOrBoy());
 
 	}
 

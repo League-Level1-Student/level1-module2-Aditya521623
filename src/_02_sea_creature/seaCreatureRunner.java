c5581ea0c3edf762820fd6692239fd33a1561c75
Aditya521623 <54295036+Aditya521623@users.noreply.github.com>
2020-06-03 16:58:20 -0700

    reviewed lessons from before

--- src/_02_sea_creature/seaCreatureRunner.java
+++ src/_02_sea_creature/seaCreatureRunner.java
@@ -8,30 +8,32 @@ public class seaCreatureRunner {
 		SeaCreature squidward = new SeaCreature("Squidward");
 
 		SeaCreature Patrick = new SeaCreature("Patrick");
-
+		
 		String name = spongebob.getName();
-
+		
 		System.out.println(name);
-
+		
 		spongebob.eat();
-
+		
 		spongebob.laugh();
-
+		
 		String name2 = squidward.getName();
-
+		
 		System.out.println(name2);
-
+		
 		squidward.eat();
-
+		
 		squidward.laugh();
-
+		
+		
 		String name3 = Patrick.getName();
-
+		
 		System.out.println(name3);
-
+		
 		Patrick.eat();
-
+		
 		Patrick.laugh();
-	}
+
+			}
 
 }

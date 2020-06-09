1a46ac47d9caec7ba7a239a97ec947f761c6f587
Aditya521623 <54295036+Aditya521623@users.noreply.github.com>
2020-06-08 19:57:57 -0700

    worked on minion test

--- src/_06_minion/Minion.java
+++ src/_06_minion/Minion.java
@@ -2,9 +2,25 @@ package _06_minion;
 
 public class Minion {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private String name; 
+		private int eyes; 
+		private String color; 
+		private String master;
 
-	}
+Minion(String name,int eyes, String color,String master){
+	this.name=name;
+	this.eyes=eyes;
+	this.color=color;
+	this.master=master;
+}
+
+String getName() {
+	return this.name;
+}
+
+void setName(String name) {
+	this.name=name;
+}
+	
 
 }

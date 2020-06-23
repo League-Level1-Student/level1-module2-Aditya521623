b3ce1f10abb15c4e36a8f67ff0e630aa45bb19c5
Aditya521623 <54295036+Aditya521623@users.noreply.github.com>
2020-06-22 19:54:25 -0700

    finished module 2

--- src/_07_tea_party/TeaParty.java
+++ src/_07_tea_party/TeaParty.java
@@ -1,11 +1,24 @@
 package _07_tea_party;
 
 public class TeaParty {
-	 public String welcome(String name, boolean isWoman, boolean isKnighted) {
-	        
-     }
-       
-        }
+	public String welcome(String name, boolean isWoman, boolean isKnighted) {
+	
+		if (isKnighted == false && isWoman == true) {
+return "Hello Ms. " + name;
+	}
 
+		else if(isKnighted==true && isWoman==true) 
+	{
+		return "Hello Lady " + name;
+	}
+	else if(isKnighted==true && isWoman==false) {
+		return "Hello Sir "+ name;
+	}
+	else
+	{
+		return "Hello Mr. "+name;
+	}
 
+}
 
+}
